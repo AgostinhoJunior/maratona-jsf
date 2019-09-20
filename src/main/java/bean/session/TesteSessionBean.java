164dffe29966e99586cbca927881290301390fb9
Agostinho Junior <45215655+AgostinhoJunior@users.noreply.github.com>
2019-09-20 18:01:40 -0300

    RequestScoped and SessionScoped

---
+++ src/main/java/bean/session/TesteSessionBean.java
@@ -0,0 +1,45 @@
+package bean.session;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Named
+@SessionScoped
+public class TesteSessionBean implements Serializable {
+
+    private List<String> personagens;
+    private List<String> personagemSelecionado = new ArrayList<>();
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Entrou no Post construct do Session Scoped");
+        personagens = Arrays.asList("Goku", "Vedita", "Gohan");
+    }
+
+    public String logout() {
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+        return "session?faces-redirect=true";
+    }
+
+    public void selecionarPersonagem() {
+        int index = ThreadLocalRandom.current().nextInt(3);
+        String personagem = personagens.get(index);
+        personagemSelecionado.add(personagem);
+    }
+
+    public List<String> getPersonagemSelecionado() {
+        return personagemSelecionado;
+    }
+
+    public void setPersonagemSelecionado(List<String> personagemSelecionado) {
+        this.personagemSelecionado = personagemSelecionado;
+    }
+
+}
